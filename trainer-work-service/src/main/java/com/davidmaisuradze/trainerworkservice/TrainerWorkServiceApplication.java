1c8313f579ecb9302479fb8d60fe48ed3b463103
davidmaisuradze <maisuradze.davit@kiu.edu.ge>
2024-05-19 22:40:33 +0400

    fixed minor warning

--- trainer-work-service/src/main/java/com/davidmaisuradze/trainerworkservice/TrainerWorkServiceApplication.java
+++ trainer-work-service/src/main/java/com/davidmaisuradze/trainerworkservice/TrainerWorkServiceApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TrainerWorkServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TrainerWorkServiceApplication.class, args);
 	}
-
 }
--- trainer-work-service/src/main/java/com/davidmaisuradze/trainerworkservice/repository/TrainerWorkSummaryRepository.java
+++ trainer-work-service/src/main/java/com/davidmaisuradze/trainerworkservice/repository/TrainerWorkSummaryRepository.java
@@ -5,13 +5,10 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TrainerWorkSummaryRepository extends JpaRepository<TrainerWorkSummary, Long> {
     @EntityGraph(attributePaths = {"yearlySummaries", "yearlySummaries.monthlySummaries"})
     Optional<TrainerWorkSummary> findByUsername(String username);
-    @EntityGraph(attributePaths = {"yearlySummaries", "yearlySummaries.monthlySummaries"})
-    List<TrainerWorkSummary> findAll();
 }
