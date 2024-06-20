c22d46fae95d5d906df57cb70814f5420b3f9448
davidmaisuradze <maisuradze.davit@kiu.edu.ge>
2024-06-20 15:19:23 +0400

    Optimize imports

--- trainer-work-service/src/test/java/com/davidmaisuradze/trainerworkservice/message/WorkloadMessageListenerSteps.java
+++ trainer-work-service/src/test/java/com/davidmaisuradze/trainerworkservice/message/WorkloadMessageListenerSteps.java
@@ -2,33 +2,17 @@ package com.davidmaisuradze.trainerworkservice.message;
 
 import com.davidmaisuradze.trainerworkservice.TrainerWorkServiceApplication;
 import com.davidmaisuradze.trainerworkservice.config.TestConfig;
-import com.davidmaisuradze.trainerworkservice.dto.ActionType;
-import com.davidmaisuradze.trainerworkservice.dto.TrainerWorkloadRequest;
-import com.davidmaisuradze.trainerworkservice.security.JwtTokenProvider;
-import com.davidmaisuradze.trainerworkservice.service.TrainerWorkSummaryService;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.apache.activemq.command.ActiveMQTextMessage;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.jms.core.JmsTemplate;
-
-import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 
 @SpringBootTest(classes = {TrainerWorkServiceApplication.class, TestConfig.class})
 public class WorkloadMessageListenerSteps {
